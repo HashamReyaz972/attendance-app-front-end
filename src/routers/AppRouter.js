61457719cf72ff1bb466783f087025f7a1711f55
HashamReyaz972 <hashamreyaz972@gmail.com>
2020-01-31 01:40:29 +0530

    clean slate

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-import Header from '../components/Header';
-import Dashboard from '../components/Dashboard'
 import Signup from '../components/Signup'
 const AppRouter = () => (
   <BrowserRouter>
     <div>-
-      <Header />
+      <Dashboard />
       <Switch>
-      <Route path="/" component={Dashboard} exact={true} />
+
       <Route path="/signup" component={Signup} />
       </Switch>
     </div>
