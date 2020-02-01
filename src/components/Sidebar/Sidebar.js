286bd06f619ca5390612d0fbc0bcab54b63d5b2e
HashamReyaz972 <hashamreyaz972@gmail.com>
2020-02-01 21:22:44 +0530

    added conditional rendering to the sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,17 +2,33 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import './_sidebar.scss'
 
-const Sidebar = () => (
+import BeforeSigninLinks from '../BeforeSigninLinks'
+import AfterSigninLinks from '../AfterSigninLinks'
+
+ const ChildrenControlLinks = [BeforeSigninLinks, AfterSigninLinks]
+
+const loggedin = true;
+
+const SidebarHOC = ([ChildOne, ChildTwo]) => {
+  return (props) => (
 
     <div className="sidepane">
-      <div className="sidepane__logo">
-        <h1>Attendance App</h1>
-      </div>
-      <div className="sidepane__inner">
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/signup">Signup</NavLink>
-        <NavLink to="/help">Help</NavLink>
-      </div>
+         <div className="sidepane__logo">
+           <h1>Attendance App</h1>
+        </div>
+
+    <div>
+      {!loggedin ? (
+        <ChildOne {...props} />
+      ) : (
+          <ChildTwo {...props} />
+        )}
     </div>
-);
+    </div>
+  );
+};
+const Sidebar = SidebarHOC(ChildrenControlLinks);
+
+
+
 export default Sidebar;
